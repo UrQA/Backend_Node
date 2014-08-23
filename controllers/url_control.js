585012cca9a992d859e6a8d7e22267ff50c2a4b9
root <root@android.(none)>
2014-08-23 15:12:34 +0900

    first commit

--- controllers/url_control.js
+++ controllers/url_control.js
@@ -1,160 +1,202 @@
+/**
+ * Created by duhyeong1.kim on 2014-08-20.
+ */
 'use strict';
 var gk = require('../common');
-var async  = require('async');
-
 var mq_pubhandler = require('../handler/mq_pubhandler');
 var queueName = gk.config.mqQueueName;
+var sprintf = require("sprintf").sprintf;
 
-var CHAR_RANGE = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz';
-exports.generateSession = function() {
-    var id_session = "";
-    for (var i = 0; i < 9; i++) {
-        var rand = Math.floor(Math.random() * CHAR_RANGE.length);
-        id_session += CHAR_RANGE.substring(rand, rand +1);
-    }
-    return id_session;
+var get_cur_time = function() {
+    var date = new Date(Date.now());
+    var year = date.getFullYear();
+    var month = date.getUTCMonth() + 1;
+    var day = date.getUTCDate();
+    var hours = date.getUTCHours();
+    var min = date.getUTCMinutes();
+    var sec = date.getUTCSeconds();
+    //# step 1 : make data
+    //# step 1 - 0 : make elementary data
+    var curtime = sprintf('%04d-%02d-%02d %02d:%02d:%02d', year, month, day, hours, min, sec);
+    return curtime;
 };
 
-exports.connect = function(req, res) {  
-  var data = { 'tag':'connect', 'instance_id':"", 'data': req.body };
-  var result = {};
-  gk.async.sequence([  //async.waterfall
-    function (cb) {
-      var ret = mq_pubhandler.publish(queueName, data);
-      //console.log("connect mq log", ret)
-      
-      var id_session = exports.generateSession();
-      result = { "idsession": id_session };
-      cb();
-    }
-  ], function (err) {
-    res.send(result);
-  });
+exports.connect = function(req, res) {
+    var data = { 'tag':'connect','data': req.body,'date_time': get_cur_time()};
 
+    //# set queue name for worker and queue
+    /*
+    var client_type = req.query.client_type;
+    var queueName = queueName;
+    if(client_type) queueName = client_type;
+    */
+    gk.async.sequence([  //async.waterfall
+        function (cb) {
+            var ret = mq_pubhandler.publish(queueName, data);
+            cb();
+        }
+    ], function (err) {
+        var result = { 'state': 'success'};
+        res.send(result);
+    });
+}
+
+exports.receive_exception = function(req, res) {
+
+    //console.log("exception Data 2 ->", req.body.exception);
+    //console.log("console log Data->", req.body.console_log);
+    var _id = req.body.instance;
+    var version = req.body.version;
+    var _data  = {};
+
+    //# set queue name for worker and queue
+    /*
+    var client_type = req.query.client_type;
+    var queueName = queueName;
+    if(client_type) queueName = client_type;
+    */
+
+    if ( version ){
+        _data.tag = 'receive_exception'
+        _data.data = req.body.exception;
+        _data.log = req.body.console_log.data;
+        _data.date_time = get_cur_time();
+        console.log("req body receive_exception--->>>> ", _data);
+    }
+    else{
+        _data.tag = 'receive_exception'
+        _data.data = req.body;
+        _data.date_time = get_cur_time();
+        console.log("req body log_data", _data);
+    }
+
+    gk.async.sequence([
+        function (cb) {
+            console.log("--->>>>>", _data);
+            mq_pubhandler.publish(queueName, _data);
+            cb();
+        }
+    ], function (err) {
+        var result = { 'state': 'success'};
+        res.send(result);
+        // console.log("return receive exception ->", result);
+    });
+}
+
+exports.receive_native = function(req, res) {
+
+    var data = { 'tag':'receive_native', 'data': req.body.exception ,'log':req.body.console_log.data,'dump_data':req.body.dump_data, 'date_time':get_cur_time()};
+
+    //# set queue name for worker and queue
+    /*
+    var client_type = req.query.client_type;
+    var queueName = queueName;
+    if(client_type) queueName = client_type;
+    */
+
+    console.log("native data ----------->" + data.data.locale);
+    //console.log("native log ----------->" + data.log);
+    //console.log("native dump data ----------->" + data.dump_data);
+    console.log("native time ----------->" + data.date_time);
+
+    gk.async.sequence([
+        function (cb) {
+            mq_pubhandler.publish(queueName, data);
+            cb();
+        }
+    ], function (err) {
+        var result = { 'state': 'success'};
+        res.send(result);
+    });
 };
 
+exports.receive_test_data = function(req, res) {
 
-exports.receive_exception = function(req, res) { 
+    var data = { 'tag':'receive_native_dump','data': req.body.img, 'date_time':get_cur_time()};
+    console.log(data.date_time);
+    console.log(req.body.img)
 
-  //console.log("exception Data 2 ->", req.body.exception);
-  //console.log("console log Data->", req.body.console_log);
-
-  var _id = req.body.instance;
-  var version = req.body.version;
-  var _data  = {};
-
-  if ( version ){
-    _data.tag = 'receive_exception'
-    _data.data = req.body.exception;
-    _data.log = req.body.console_log.data;
-    console.log("req body receive_exception--->>>> ", _data);
-  }
-  else{
-    _data.tag = 'receive_exception'
-    _data.data = req.body;
-    console.log("req body log_data", _data);
-  }
-
-  var result = {};
-  gk.async.sequence([
-    function (cb) {
-      console.log("--->>>>>", _data); 
-      mq_pubhandler.publish(queueName, _data);
-      cb();
-    }
-  ], function (err) {
-    result = { 'state': 'success'};
-    res.send(result);
-    // console.log("return receive exception ->", result);
-  });
-};
-
-
-exports.receive_native_dump = function(req, idinstance, res) {  
-  var data = { 'tag':'receive_native_dump', 'instance_id':instance, 'data': req.body };
-  var result = {};
-  gk.async.sequence([
-    function (cb) {
-      mq_pubhandler.publish(queueName, data);
-      cb();
-    }
-  ], function (err) {
-    result = { 'state': 'success'};
-    res.send(result);
-  });
+    //# set queue name for worker and queue
+    /*
+    var client_type = req.query.client_type;
+    var queueName = queueName;
+    if(client_type) queueName = client_type;
+    */
+    gk.async.sequence([
+        function (cb) {
+            mq_pubhandler.publish(queueName, data);
+            cb();
+        }
+    ], function (err) {
+        var result = { 'state': 'success'};
+        res.send(result);
+    });
 
 };
-
-
-exports.receive_native = function(req, res) {  
-  var data = { 'tag':'receive_native', 'instance_id':"", 'data': req.body };
-  var result = {};
-  
-  gk.async.sequence([
-    function (cb) {
-      mq_pubhandler.publish(queueName, data);
-      cb();
-    }
-  ], function (err) {
-    result = { 'state': 'success'};
-    res.send(result);
-  });
-
-};
-
-
 
 exports.url_redirect = function(req,res) {
- // res.redirect('http://ur-qa.com:9000');
-   gk.async.sequence([
-    function (cb) {
-      res.writeHead(301,
-        { 
-          Location: 'http://ur-qa.com:9000/' 
-        });
-        res.end();    
-    }
-  ], function (err) {
-    res.send(result);
-  });
+    // res.redirect('http://ur-qa.com:9000');
+    gk.async.sequence([
+        function (cb) {
+            res.writeHead(301,
+                {
+                    Location: 'http://urqa.io'
+                });
+            res.end();
+        }
+    ], function (err) {
+        res.send(result);
+    });
 };
-
 
 /*
-exports.receive_eventpath = function(req, res) {  
+ exports.receive_native_dump = function(req, res) {
 
-  var data = { 'tag':'receive_eventpath','instance_id':"", 'data': req.body };  
-  var result = {};
+ var dumpId = req.query.dumpId;
+ //var buffer = BSON.serialize(req.body)
+ //# step 1 - 1 make binary
+ var binaryData= ''
+ req.setEncoding('binary')
 
-  async.waterfall([
-    function (cb) {
-      mq_pubhandler.publish(queueName, data);
-      cb();
-    }
-  ], function (err) {
-    result = { 'state': 'success'};
-    res.send(result);
-  });
+ req.on('data', function(chunk){
+ binaryData += chunk
+ });
 
-};
-*/
+ req.on('end', function(){
+ console.log('File saved.');
+ //# step 1 -2 : this is process to input binaryData into json
+ //var encodedData = BSON.serialize(binaryData);
+ var encodedData = binaryData;
+
+ var data = {'tag':'receive_native_dump','data':encodedData,'date_time':get_cur_time()};
+ console.log("data ------------->"+data.data);
+ console.log("data type -------->" + typeof(data.data));
+ //# step 2 : send data to rabbit mq
+ gk.async.sequence([
+ function (cb) {
+ console.log("before pub");
+ mq_pubhandler.publish(queueName, data);
+ cb();
+ console.log("receive_native_dump");
+ }
+ ], function (err) {
+ res.send("success");
+ });
+
+ });
+ };
+
+ exports.generateSession = function() {
+ var id_session = "";
+ for (var i = 0; i < 9; i++) {
+ var rand = Math.floor(Math.random() * CHAR_RANGE.length);
+ id_session += CHAR_RANGE.substring(rand, rand +1);
+ }
+ return id_session;
+ };
+ */
 
 
-/*
-exports.receive_exception_log = function(req, idinstance, res) {
-  var data = { 'tag':'receive_exception_log', 'instance_id':instance, 'data': req.body }; 
-  var result = {};
-  
-  async.waterfall([  
-    function (cb) {
-      mq_pubhandler.publish(queueName, data);
-      cb();
-    }
-  ], function (err) {
-    result = { 'state': 'success'};
-    res.send(result);
-  });
-};
-*/
+
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,40 @@
+/**
+ * Created by duhyeong1.kim on 2014-08-20.
+ */
 'use strict'
 var url_control = require(__dirname + '/../controllers/url_control');
-
 var routes = function(app) {
-  
-  //client module
-  app.post('/urqa/client/connect', url_control.connect);
-  app.post('/urqa/client/send/exception', url_control.receive_exception);
-  app.post('/urqa/client/send/exception/native', url_control.receive_native);
-  app.post('/urqa/client/send/exception/dump/(?P<idinstance>\d+)$', url_control.receive_native_dump);
-  //app.post('/urqa/client/send/exception/log/(?P<idinstance>\d+)$', url_control.receive_exception_log);
-  //app.post('/urqa/client/send/eventpath', url_control.receive_eventpath);
+    /*
+    this is for legact rest apis for android
+     */
+    app.post('/urqa/client/connect', url_control.connect);
+    app.post('/urqa/client/send/exception', url_control.receive_exception);
+    app.post('/urqa/client/send/exception/native', url_control.receive_native);
+    app.post('/test/',url_control.receive_test_data);
+    /*
+    this is for new rest apis for android, ios, unity, cordova
+     */
+    /*
+    app.post('/urqa/client/android/connect', url_control.connect_4_android);
+    app.post('/urqa/client/android/exception', url_control.receive_exception_4_android);
+    app.post('/urqa/client/android/exception/native', url_control.receive_native_4_android);
 
-   app.get('/$',url_control.url_redirect);
-   app.post('/urqa', url_control.url_redirect);
+    app.post('/urqa/client/ios/connect', url_control.connect_4_ios);
+    app.post('/urqa/client/ios/exception', url_control.receive_exception_4_ios);
+    app.post('/urqa/client/ios/exception/native', url_control.receive_native_4_ios);
+
+    app.post('/urqa/client/unity/connect', url_control.connect);
+    app.post('/urqa/client/unity/exception', url_control.receive_exception);
+    app.post('/urqa/client/unity/exception/native', url_control.receive_native);
+
+    app.post('/urqa/client/cordova/connect', url_control.connect);
+    app.post('/urqa/client/cordova/exception', url_control.receive_exception);
+    app.post('/urqa/client/cordova/exception/native', url_control.receive_native);
+    */
+    app.get('/$',url_control.url_redirect);
+    app.post('/urqa', url_control.url_redirect);
 }
 exports.route = routes;
+
+
+
