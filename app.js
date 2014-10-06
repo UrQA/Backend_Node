e552b80350ef8717892f459849548336c45a8777
서병선 <famersbs@gmail.com>
2014-10-06 10:03:48 +0900

    암호화 테스트중

--- app.js
+++ app.js
@@ -19,5 +19,44 @@ var app = express();
 // router
 var router = require('./routes');
 router.route(app);
+
+// encrypt test
+app.get('/test_enc', function( req, res ){
+
+	var uri = req.query.uri;	// 이 부분을 변경 해야 할듯 --> 형식을 바꿀필요가 있음
+	var data = req.query.data;
+
+	/* https://www.npmjs.org/package/crypto-js */
+	var CryptoJS = require("crypto-js");
+
+	var INITIALIZE_VECTOR = '0000000000000000';
+	var CHIPER_TYPE = 'aes-256-cbc';
+	var ENCODE_TYPE = 'base64';
+
+	var HASH_TYPE         = 'sha256';
+	var HASH_UPDATE_DATA  = 'urqa_service_NumberONE!!!';
+	var KEY = CryptoJS.SHA256( HASH_UPDATE_DATA );
+
+	console.log( CryptoJS.enc.Hex.parse( INITIALIZE_VECTOR ) );
+
+	var iv  = CryptoJS.enc.Hex.parse( INITIALIZE_VECTOR );
+
+	var aec_Encrypter = CryptoJS.algo.AES.createEncryptor( KEY, { iv: iv } );
+
+	var result = aec_Encrypter.process( data ) + aec_Encrypter.finalize();
+
+
+	//AES.encrypt( KEY, data, { iv:INITIALIZE_VECTOR } );
+	reesult = CryptoJS.enc.Base64.stringify(result);
+
+	console.log( result );
+
+
+	res.header('Content-type','application/json');
+	res.header('Charset','utf8');
+	res.jsonp( { result:data} );
+
+});
+
 app.listen(gk.config.port);
 console.log("Server started at port " + gk.config.port);
