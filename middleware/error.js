3589d50a63575cf305b22b5c14a59ffc4a94f505
pegasuskim <pegasuskim@gmail.com>
2015-01-25 23:22:07 +0900

    pegasus index.html delete

--- middleware/error.js
+++ middleware/error.js
@@ -1,15 +1,8 @@
 'use strict';
 
 module.exports = function() {
-
   return function(err, req, res, next) {
-<<<<<<< HEAD
-      //console.log('aa');
-=======
-      //console.log(err, req.body);
-      //console.log( err.stack );
-      console.log("error ", err.stack );
->>>>>>> c042354a52ad7f7fede496c14c5c420b73935840
+      //console.log("error ", err.stack );
       if (err) {
           return res.send(500, 'Error' + err.stack);
       }
