2f94d48a964c5f0e7e9982740e76a746af123f28
pegasuskim <pegasuskim@gmail.com>
2015-10-17 14:26:57 +0900

    config arranged

--- config/index.js
+++ config/index.js
@@ -4,11 +4,13 @@ var os = require('os');
 var hostname = os.hostname();
 //console.log("hostname:"+ hostname);
 
+var config = require('./develop.json');
+
 if (hostname == 'A12038') {
   // This config is for lhs    
 } else {
   // This config is for develop server
-  var config = require('./develop.json');
+  config = require('./develop.json');
 }
 
 
